1122c1d568b1264dab76f2a3ce56ebf504f61162
RayanSf18 <rayansf18@gmail.com>
2023-12-19 08:27:27 -0300

    subindo exceção

--- exercise-reflection/src/application/Program.java
+++ exercise-reflection/src/application/Program.java
@@ -5,18 +5,23 @@ import annotations.Tabela;
 public class Program {
     public static void main(String[] args) throws ClassNotFoundException {
 
-        // Get the Customer class
-        Class<?> customerClass = Class.forName("entities.Customer");
+        try {
+            // Get the Customer class
+            Class<?> customerClass = Class.forName("entities.Customer");
 
-        // Check for Tabela annotation on the class
-        Tabela tabelaAnnotation = customerClass.getAnnotation(Tabela.class);
+            // Check for Tabela annotation on the class
+            Tabela tabelaAnnotation = customerClass.getAnnotation(Tabela.class);
 
-        if (tabelaAnnotation != null) {
-            // Print the name of the Tabela annotation
-            System.out.println("Tabela name: " + tabelaAnnotation.name());
-        } else {
-            System.out.println("Customer class does not have Tabela annotation!");
+            if (tabelaAnnotation != null) {
+                // Print the name of the Tabela annotation
+                System.out.println("Tabela name: " + tabelaAnnotation.name());
+            } else {
+                System.out.println("Customer class does not have Tabela annotation!");
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: Class not found!");
         }
+
 
     }
 }
